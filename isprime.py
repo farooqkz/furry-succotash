f18af9b97141ec026d87f079aa0eda2147b7686d
Farooq Karimi Zadeh <fkz@riseup.net>
2021-04-29 23:14:36 +0430

    Update isprime.py

--- isprime.py
+++ isprime.py
@@ -1,5 +1,5 @@
 def isprime(n):
-    if isinstance(n, int):
+    if not isinstance(n, int):
         raise TypeError("n must be an instance of integer")
     if n < 1:
         raise ValueError("n must be a positive greater than or equal to 1")
