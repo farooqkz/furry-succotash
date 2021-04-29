7c0b5394c4c3c82ebdb61d2c74c50d9bbbb9c7a2
Farooq Karimi Zadeh <fkz@riseup.net>
2021-04-29 21:16:05 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    n = int(input("Please enter a positive number: "))
+    n = int(input("Please enter a positive number(some more text to make a long string and cause failure of linting: "))
     for i in range(n):
         for j in range(i):
             print("=" * j)
