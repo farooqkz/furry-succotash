4770a115ef2729e22f54775aecc8024db80cd25a
Farooq Karimi Zadeh <fkz@riseup.net>
2021-04-29 23:15:54 +0430

    Update isprime.py

--- isprime.py
+++ isprime.py
@@ -3,7 +3,7 @@ def isprime(n):
         raise TypeError("n must be an instance of integer")
     if n < 1:
         raise ValueError("n must be a positive greater than or equal to 1")
-    for i in range(n):
+    for i in range(2, n):
         if n%i == 0:
             return False
     return True
